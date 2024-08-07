1fb685bd8b3160dd65ee4c75e2817d4ec758f4c9
dharshh-31 <118419457+dharshh-31@users.noreply.github.com>
2024-08-07 10:03:41 +0530

    Add files via upload

---
+++ BACKEND/ContactRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.model.ContactModel;
+
+public interface ContactRepository extends JpaRepository<ContactModel, Long> {
+}
